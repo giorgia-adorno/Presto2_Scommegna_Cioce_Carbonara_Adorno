d1e218f0060a25fb05872546e1dd4646994f5221
Michele Scommegna <michelescommegna63@gmail.com>
2026-06-24 15:22:37 +0200

    scuffholding done

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,31 @@
+<footer class="bg-light text-center text-lg-start mt-auto">  
+    <div class="container p-4">  
+        <div class="row">  
+            <div class="col-lg-6 col-md-12 mb-4 mb-md-0">  
+                <h5 class="text-uppercase">MioProgetto</h5>  
+                <p>  
+                    Descrizione breve del progetto. Questo footer è statico e veloce.  
+                </p>  
+            </div>  
+  
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">  
+                <h5 class="text-uppercase">Link Utili</h5>  
+                <ul class="list-unstyled mb-0">  
+                    <li><a href="#!" class="text-dark">Link 1</a></li>  
+                    <li><a href="#!" class="text-dark">Link 2</a></li>  
+                </ul>  
+            </div>  
+  
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">  
+                <h5 class="text-uppercase">Contatti</h5>  
+                <ul class="list-unstyled mb-0">  
+                    <li><a href="#!" class="text-dark">Supporto</a></li>  
+                </ul>  
+            </div>  
+        </div>  
+    </div>  
+  
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">  
+        © {{ date('Y') }} Copyright: <a class="text-dark" href="{{ url('/') }}">MioProgetto.it</a>  
+    </div>  
+</footer>  
